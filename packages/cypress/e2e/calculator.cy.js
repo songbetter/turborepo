11b833a436edb04f725bab99fc350c03ca1ada93
songbetter <5ongnaeu17@gmail.com>
2022-12-19 21:31:06 +0900

    test: Calculator cypress test

---
+++ packages/cypress/e2e/calculator.cy.js
@@ -0,0 +1,55 @@
+describe('example calculator app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000')
+  })
+
+  it('최초에 계산기 값을 0으로 보여준다', () => {
+    cy.get('#calculator_result').invoke('text').should('eq', '0')
+  })
+
+  it('AC(All Clear)버튼을 누르면 0으로 초기화 한다.', () => {
+    cy.get('.operator').contains('AC').click()
+
+    cy.get('#calculator_result').invoke('text').should('eq', '0')
+  })
+
+  it('2개의 숫자에 대해 덧셈이 가능하다.', () => {
+    cy.get('.digit').contains('2').click()
+    cy.get('.operator').contains('+').click()
+    cy.get('.digit').contains('4').click()
+    cy.get('.operator').contains('=').click()
+    cy.get('#calculator_result').should('have.text', '6')
+  })
+
+  it('2개의 숫자에 대해 뺄셈이 가능하다.', () => {
+    cy.get('.digit').contains('3').click()
+    cy.get('.operator').contains('-').click()
+    cy.get('.digit').contains('2').click()
+    cy.get('.operator').contains('=').click()
+    cy.get('#calculator_result').should('have.text', '1')
+  })
+
+  it('2개의 숫자에 대해 곱셈이 가능하다.', () => {
+    cy.get('.digit').contains('3').click()
+    cy.get('.operator').contains('X').click()
+    cy.get('.digit').contains('2').click()
+    cy.get('.operator').contains('=').click()
+    cy.get('#calculator_result').should('have.text', '6')
+  })
+
+  it('2개의 숫자에 대해 나눗셈이 가능하다.', () => {
+    cy.get('.digit').contains('9').click()
+    cy.get('.operator').contains('/').click()
+    cy.get('.digit').contains('3').click()
+    cy.get('.operator').contains('=').click()
+    cy.get('#calculator_result').should('have.text', '3')
+  })
+
+  it('계산 결과를 표현할 때 소수점 이하는 버림한다.', () => {
+    cy.get('.digit').contains('3').click()
+    cy.get('.operator').contains('/').click()
+    cy.get('.digit').contains('2').click()
+    cy.get('.operator').contains('=').click()
+    cy.get('#calculator_result').should('have.text', '1')
+  })
+})
--- packages/ui/Calculator.tsx
+++ packages/ui/Calculator.tsx
@@ -1,4 +1,89 @@
+import React, { useState } from 'react'
+
 export const Calculator = () => {
+  const [result, setResult] = useState(INITIAL_VALUE)
+  const [total, setTotal] = useState(0)
+
+  function reset() {
+    setResult(INITIAL_VALUE)
+    setTotal(0)
+  }
+
+  function calculator() {
+    let calculateResult = result.currentValue
+
+    switch (result.operator) {
+      case '+': {
+        calculateResult += result.temp
+        break
+      }
+      case 'X': {
+        calculateResult *= result.temp
+        break
+      }
+      case '-': {
+        calculateResult -= result.temp
+        break
+      }
+      case '/': {
+        calculateResult /= result.temp
+        break
+      }
+      default: {
+        break
+      }
+    }
+    setResult({
+      ...result,
+      currentValue: calculateResult,
+      temp: 0,
+      operator: '',
+    })
+    setTotal(calculateResult)
+  }
+  function finish() {
+    calculator()
+  }
+
+  function setOperation(operator: string) {
+    if (result.currentValue && result.temp) {
+      calculator()
+      setTotal(result.currentValue)
+    }
+    setResult({ ...result, operator })
+  }
+
+  function setNumber(value) {
+    if (result.operator) {
+      const temp = Number(String(result.temp) + value)
+      setResult({ ...result, temp })
+      setTotal(result.temp)
+    } else {
+      const currentValue = Number(String(result.currentValue) + value)
+      setResult({ ...result, currentValue })
+      setTotal(result.currentValue)
+    }
+  }
+  function handleClickBtn(e: React.MouseEvent<HTMLElement>) {
+    const { innerText: value } = e.target
+    // 숫자라면
+    if (/[0-9]/.test(value)) {
+      return setNumber(value)
+    }
+
+    // 초기화라면
+    if (value === 'AC') {
+      return reset()
+    }
+
+    // 계산 끝
+    if (value === '=') {
+      return finish()
+    }
+
+    // 계산하는 값들이라면
+    return setOperation(value)
+  }
   return (
     <div
       style={{
@@ -10,46 +95,144 @@ export const Calculator = () => {
       <div
         style={{
           gridColumn: '1/-1',
-          fontSize: '5rem',
           margin: 'auto 0',
           textAlign: 'end',
         }}
       >
-        0
-      </div>
-      {CALCULATOR.map((data, idx) => (
-        <button
-          key={data}
+        <div>
+          {result.currentValue}
+          {result.operator && result.operator}
+          {result.temp !== 0 && result.temp}
+        </div>
+        <div
           style={{
-            gridColumn:
-              idx === 0 || idx === CALCULATOR.length - 1 ? 'span 2' : '',
-            fontSize: '2rem',
+            fontSize: '5rem',
+            overflow: 'auto',
           }}
+          id="calculator_result"
         >
-          {data}
-        </button>
-      ))}
+          {Math.floor(total)}
+        </div>
+      </div>
+      <button
+        style={{ gridColumn: 'span 3', fontSize: '2rem' }}
+        onClick={handleClickBtn}
+        className="operator"
+      >
+        AC
+      </button>
+      <button
+        className="operator"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        /
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        1
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        2
+      </button>
+
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        3
+      </button>
+      <button
+        className="operator"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        X
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        4
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        5
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        6
+      </button>
+      <button
+        className="operator"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        +
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        7
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        8
+      </button>
+      <button
+        className="digit"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        9
+      </button>
+      <button
+        className="operator"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        -
+      </button>
+      <button
+        className="digit"
+        style={{ gridColumn: 'span 3', fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        0
+      </button>
+      <button
+        className="operator"
+        style={{ fontSize: '2rem' }}
+        onClick={handleClickBtn}
+      >
+        =
+      </button>
     </div>
   )
 }
 
-const CALCULATOR = [
-  'AC',
-  'DEL',
-  '%',
-  1,
-  2,
-  3,
-  'X',
-  '4',
-  '5',
-  '6',
-  '+',
-  '7',
-  '8',
-  '9',
-  '-',
-  '.',
-  '0',
-  '=',
-]
+const INITIAL_VALUE = {
+  currentValue: 0,
+  temp: 0,
+  operator: '',
+}
